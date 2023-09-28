63680e73625d401e28980dc9e040556d1f180c71
Azzam ALmasslamany <azzammeri24@gmail.com>
2023-09-28 15:32:31 +0300

    I Write the full code here

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I will ${activity}!`;
+}
+
+function mondayWork(task = 'go to the office') {
+  return `On Monday, I have to ${task}.`;
+}
+
+function wrapAdjective(flair = '*') {
+  return function(adjective = 'special') {
+    return `You are ${flair}${adjective}${flair}!`;
+  }
+}
+
+const encouragingPromptFunction = wrapAdjective("%");
+console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are %a dedicated programmer%!"
